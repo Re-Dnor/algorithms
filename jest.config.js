bf16782d35bfbaaabeeafcad54ff0fd756328df5
Mokshin Alexandr <awmokshin@gmail.com>
2023-03-21 13:59:15 +0300

    add jest and babel

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bail: false,
+  verbose: true,
+  collectCoverage: false,
+  coverageDirectory: "./coverage/",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/"],
+};
