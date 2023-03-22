2b4197f85226b51057290f6fad3d1c09fbc3bf5d
Mokshin Alexandr <MokshinRe@yandex.ru>
2023-03-22 15:58:11 +0300

    add consts

---
+++ src/search/utils/index.js
@@ -0,0 +1,11 @@
+export const sortedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const unsortedNumbers = [10, 1, 2, 25, 0, 7, 9, 8, 13];
+
+export const bigSortedNumbers = new Array(10000).fill(0).map((_, i) => i + 1);
+
+export const missingNumber = [1, 2, 3, 5, 6, 7, 8, 9, 10];
+
+export const aloneNumber = [1];
+
+export const emptyArray = [];
